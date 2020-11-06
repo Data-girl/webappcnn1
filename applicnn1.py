0d477ded3ed11da7379e9b5bbae0979893c31e3e
Data-girl <67791101+Data-girl@users.noreply.github.com>
2020-11-06 20:43:38 +0100

    Update applicnn1.py

--- applicnn1.py
+++ applicnn1.py
@@ -57,9 +57,9 @@ def run():
     st.sidebar.image(image2, use_column_width=True)
     
     #Personnalisation page centrale
-    st.sidebar.success("Vous souhaitez concevoir votre propre interface de prédiction afin de détecter les cas de cancer du poumons ? Retrouvez-nous sur http://www.agence-marketic.fr")
+    st.sidebar.success("Vous souhaitez concevoir votre propre interface de prédiction afin de détecter les cas de cancer des poumons ? Retrouvez-nous sur http://www.agence-marketic.fr")
     
-    st.write("Cette interface à pour objectif de faciliter la détection du cancer des poumons. Elle a été conçue à partir d'une base de données de 6000 références de scanner des poumons (sains et cancéreux), et offre une **précision de 99%** dans l'identification des patients atteints par la maladie. Le modèle de prédiction repose sur un réseau de neurones à reconnaissance d'image, aussi connu sous l'appellation de réseaux de neurones convolutifs (CNN).")
+    st.write("Cette interface a pour objectif de faciliter la détection du cancer des poumons. Elle a été conçue à partir d'une base de données de 6000 références de scanner des poumons (sains et cancéreux), et offre une **précision de 99%** dans l'identification des patients atteints par la maladie. Le modèle de prédiction repose sur un réseau de neurones à reconnaissance d'image, aussi connu sous l'appellation de réseaux de neurones convolutifs (CNN).")
     
     st.write(""" [🟢 - Cliquez ici pour réaliser un test en téléchargeant une image de poumons présentant un état sain](https://drive.google.com/uc?export=download&id=1Muzi-Fzf0z4B81Tcpd_5gvDwkQkl40GM)""")
     st.write(""" [🔴 - Cliquez ici pour réaliser un test en téléchargeant une image de poumons présentant un état cancéreux](https://drive.google.com/uc?export=download&id=1Df7eDWDR1hxsTZLGpLYoZbV6BC0uIV8w)""")
